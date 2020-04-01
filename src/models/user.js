82dc17ebe32e0b638e4fbdca3664d46590b61dad
Bibin <bibinjoseph.dev@gmail.com>
2020-04-01 17:37:20 +0530

    Models for resource endpoints

---
+++ src/models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true //to filter white spaces in name
+    }, 
+    email: {
+        type: String,
+        required: true,
+        lowercase: true, //for lowercase
+        validate(value) {
+            if (!validator.isEmail(value))
+                throw new Error('Invalid email entered')
+        }
+    },
+    age: {
+        type: Number,
+        default: 18, //default value for age
+        validate(value) {
+            if (value < 18) {
+                throw new Error('Age must be at least 18')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validate(value) {
+            console.log(value)
+            if (value.toLowerCase().includes('password'))
+                throw new Error('Password cannot include \'password\'')
+        }
+    }
+})
+
+module.exports = User
