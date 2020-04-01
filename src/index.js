48e5cc8db6818bb03c0d6385c6eb3e442e1527b2
Bibin <bibinjoseph.dev@gmail.com>
2020-04-01 18:14:53 +0530

    Added resource endpoint for fetching tasks and users

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const User = require('./models/user')
 const Task = require('./models/task')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 app.use(express.json())
 
@@ -25,6 +25,54 @@ app.post('/tasks', (req, res) => {
         res.send(task)
     }).catch((error) => {
         res.status(400).send(error)
+    })
+})
+
+// fetch all users
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+// fetch user by ID 
+// (can also use findOne() if you want to search by any criteria other than ID)
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id //for accessing the request params
+    User.findById(_id).then((user) => {
+        if (!user) 
+            return res.status(404).send()
+        
+        res.send(user)
+    }).catch((error) => {
+        res.status(500).send(error)
+    })
+})
+
+// fetch all tasks
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        if (!tasks || tasks.length === 0)
+            return res.status(404).send([])
+
+        res.send(tasks)
+    }).catch((error) => {
+        res.status(500).send(error)
+    })
+})
+
+// fetch task by ID
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+    Task.findById(_id).then((task) => {
+        if (!task)
+            return res.status(404).send()
+        
+        res.send(task)
+    }).catch((error) => {
+        res.status(500).send(error)
     })
 })
 
