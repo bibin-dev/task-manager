388430ae3d5806b690f2b0cd00f4551de9589902
Bibin <bibinjoseph.dev@gmail.com>
2020-04-01 17:38:09 +0530

    Setting up resource endpoints for user and task model

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+require('./db/mongoose')
+const User = require('./models/user')
+const Task = require('./models/task')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.post('/users', (req, res) => {
+    const user = new User(req.body)
+    user.save().then(() => {
+        res.send(user)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+    // console.log(req.body)
+    // res.send('Testing perfect')
+})
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+    task.save().then(() => {
+        res.send(task)
+    }).catch((error) => {
+        res.status(400).send(error)
+    })
+})
+
+app.listen(port, () => {
+    console.log('server is up on port ' + port)
+})
+
